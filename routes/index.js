e528bb1b6f5b54bed4ace59cd6de989c8698c1a0
shanurrahman <rshanur@deepiq.com>
2019-05-30 02:26:30 +0530

    in memory testing of slack api
    will add the persistence layer later; could use a simple json file or maybe mongodb database

--- routes/index.js
+++ routes/index.js
@@ -1,24 +1,30 @@
 var express = require("express");
 var router = express.Router();
-
+var items = [];
 /* GET home page. */
 router.post("/", function(req, res, next) {
   res.status(200).send("Post request @Home route" + req.url);
 });
 
 router.post("/addtodo", function(req, res, next) {
-  res.status(200).send("/addtodo POST");
+  let itemsArray = req.body.text.split(" ");
+  items = items.concat(itemsArray);
+  res.status(200).send(items);
 });
 
-
-
-router.post("/marktodo", function (req, res, next) {
+router.post("/marktodo", function(req, res, next) {
   res.status(200).send("/marktodo POST");
-})
+});
 
-router.post("/listtodo", function (req, res, next) {
-  res.status(200).send("/listtodo POST");
-})
+router.post("/listtodo", function(req, res, next) {
+  res.status(200).send(items);
+});
 
+router.post("/removetodo", function(req, res, next) {
+  let delItem = req.body.text;
+  items = items.filter(item => item !== delItem);
+
+  res.status(200).send(items);
+});
 
 module.exports = router;
