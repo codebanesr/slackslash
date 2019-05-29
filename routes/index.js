1475a644f652d0c938fdfe21bf02b70a08bf3d3d
shanurrahman <rshanur@deepiq.com>
2019-05-30 01:56:39 +0530

    initial setup done and successful

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.post("/", function(req, res, next) {
-  res.status(200).send("Post request @Home route");
-});
-
-router.get("/addtodo", function(req, res, next) {
-  res.status(200).send("Hello there");
+  res.status(200).send("Post request @Home route" + req.url);
 });
 
 router.post("/addtodo", function(req, res, next) {
-  res.status(200).send("Post request received at /addtodo");
+  res.status(200).send("/addtodo POST");
 });
+
+
+
+router.post("/marktodo", function (req, res, next) {
+  res.status(200).send("/marktodo POST");
+})
+
+router.post("/listtodo", function (req, res, next) {
+  res.status(200).send("/listtodo POST");
+})
+
 
 module.exports = router;
