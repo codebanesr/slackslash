dfeede3355407d81aa2e80ee34ed66c01492fb84
shanurrahman <rshanur@deepiq.com>
2019-06-02 07:29:48 +0530

    adding the start script

---
+++ start.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export webhook=https://hooks.slack.com/services/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
+export dbUrl=mongodb://dev:devkumar123@ds117422.mlab.com:17422/datavalidation
+
+npm start
